0e6837040e5651faa6e3ae5c004ae94b1bcbfa64
Casper Shih <casshih@gmail.com>
2018-10-06 08:31:31 -0700

    heroku

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ var bcrypt = require('bcryptjs');
 const db2 = require("./db/db.js");
 
 
+//Serve static assets when the app is running on production
+if(process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 // Init App
 const app = express();
 var PORT = process.env.PORT || 5000;
