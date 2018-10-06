04f6c07293fb813cc6ce1906fc16c1ec92a6e895
Casper Shih <casshih@gmail.com>
2018-10-06 08:43:09 -0700

    fix

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const db2 = require("./db/db.js");
 //Serve static assets when the app is running on production
 if(process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("/client/build"));
+  app.use(express.static("/client/public"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
   });
 }
 
